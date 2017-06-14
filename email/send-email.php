ce3b2ff4cc0cac6be49991ae7620b856cc7bd9a0
windsor80 <windsorjunior@hotmail.com>
2017-06-13 20:22:01 -0400

    updates

--- email/send-email.php
+++ email/send-email.php
@@ -27,6 +27,6 @@ $sg = new \SendGrid($apiKey);
 /*Response*/
 $response = $sg->client->mail()->send()->post($mail);
 
-
+echo 'Thanks for your message, I will get in touch with you as soon as possible!'
 ?>
 
