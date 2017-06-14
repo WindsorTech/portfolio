0fc6f291751ffaa6bfc23a88d6d09d7c4716f6ff
windsor80 <windsorjunior@hotmail.com>
2017-06-13 22:23:50 -0400

    updates

--- email/send-email.php
+++ email/send-email.php
@@ -28,14 +28,95 @@ $sg = new \SendGrid($apiKey);
 $response = $sg->client->mail()->send()->post($mail); 
 ?>
 
-<?php
+<html>
+<head>
+  <title>Windsor Edeling</title>
 
-echo "<br>";
-echo "<center><h1>Thank You ".$name." for your message!</h1></center>";
-echo "<br>";
-echo "<center><h2>I will get in touch as soon as possible.</h2></center>";
-echo "<br>";
-echo "<center><h2><a href='/portfolio.html'>Click Here</a> to return to the page.</h2></center>";
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-?>
+  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cerulean/bootstrap.min.css" rel="stylesheet" integrity="sha384-zF4BRsG/fLiTGfR9QL82DrilZxrwgY/+du4p/c7J72zZj+FLYq4zY00RylP9ZjiT" crossorigin="anonymous">
+
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+
+    <div> <!-- NAVBAR -->
+        <div class="navbar text-center">
+          <h2 class="center-pills">Windsor Roberto Edeling</h2>
+        </div>
+    </div> <!-- NAVBAR -->
+
+    <div class="text-center">
+          <ul class="nav nav-pills center-pills">
+            <li class="active"><a href="index.html">About</a></li>
+            <li class="active"><a href="portfolio.html">Portfolio</a></li>
+            <li class="active"><a href="contact.html">Contact</a></li>
+          </ul>
+    </div>
+
+<div class="container">
+
+  <div class="title"> <!-- HEADER -->
+    <h1>Contact</h1>
+  </div>
+  
+    <div class="row">
+
+    <div class="col-md-9 well">
+
+        <form action="email/send-email.php" method="post">
+          
+            <div class="form-group contact">
+              <label for="form_name" class="col-lg-1 control-label">Name</label>
+              <div class="col-lg-12">
+              <input type="text" class="form-control" name="name" placeholder="Name">
+              </div>
+            </div> 
+
+            <div class="form-group contact">
+              <label for="email" class="col-lg-1 control-label">Email</label>
+              <div class="col-lg-12">
+              <input type="text" class="form-control" name="email" placeholder="Email">
+              </div>
+            </div>
+
+            <div class="form-group contact">
+              <label for="message" class="col-lg-1 control-label">Message</label>
+              <div class="col-lg-12">
+                <textarea type="text" class="form-control" name="message" rows="5" placeholder="Write Your Message Here"></textarea>
+              </div>
+            </div> 
+            
+            <div class="form-group">
+              <div class="col-lg-12 send-btn">
+              <h1>THANK YOU FOR THE MESSAGE!</h1>
+              </div>
+            </div>
+        
+      </form>
+  </div>
+  
+
+
+      <div class="col-md-3">
+        <div class="socialbuttons well">
+          <center>Let's Connect</center>
+          <hr>
+          <center><a href="https://www.linkedin.com/in/windsoredeling" target="_blank"> <img src="images/linkedin-icon.png" width="50" height="50"></a> <a href="https://github.com/windsor80" target="_blank"><img src="images/github-icon.png" width="50" height="50"></a> <a href="https://facebook.com/windsor.roberto" target="_blank"><img src="images/facebook-icon.png" width="50" height="50"></a></center>
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+
+    <div class="footer"> <!--FOOTER-->
+    Copyright 2017 Windsor Edeling
+    </div>
+</body>
+</html>
+
 
