4c8ccdeb1aa69e3b7afc5170f2c58b85dfc5b496
windsor80 <windsorjunior@hotmail.com>
2017-06-13 20:57:44 -0400

    updates

--- email/send-email.php
+++ email/send-email.php
@@ -27,8 +27,8 @@ $sg = new \SendGrid($apiKey);
 /*Response*/
 $response = $sg->client->mail()->send()->post($mail);
 
-$sucesso = "Thanks for your message, I will get in touch with you as soon as possible!"
+echo "<div class='result'> Thanks for your message, I will get in touch with you as soon as possible! </div>";
 
-var_dump($sucesso);
+
 ?>
 
