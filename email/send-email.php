cfd299ca8de1dd3c6fdaa65908658ce4ec4c5204
windsor80 <windsorjunior@hotmail.com>
2017-06-13 22:12:52 -0400

    updates

--- email/send-email.php
+++ email/send-email.php
@@ -30,10 +30,12 @@ $response = $sg->client->mail()->send()->post($mail);
 
 <?php
 
-echo "<h1>Thank You for your message</h1>";
 echo "<br>";
-echo "<h2>I will get in touch as soon as possible";
-echo "<button class='btn btn-primary'><a href='/portfolio.html'>Return Home</a></button>";
+echo "<center><h1>Thank You for your message!</h1></center>";
+echo "<br>";
+echo "<center><h2>I will get in touch as soon as possible.</h2></center>";
+echo "<br>";
+echo "<center><h2><a href='/portfolio.html'>Click Here</a>to return to the page.</h2></center>";
 
 ?>
 
