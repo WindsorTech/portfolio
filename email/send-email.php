c9025e9cb5d84b8f9972283b0d5b930ff491ce53
windsor80 <windsorjunior@hotmail.com>
2017-07-21 01:51:51 -0400

    latest updates

--- email/send-email.php
+++ email/send-email.php
@@ -39,6 +39,7 @@ $response = $sg->client->mail()->send()->post($mail);
 
   <link rel="stylesheet" type="text/css" href="/css/style.css">
 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
